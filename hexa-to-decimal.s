24bc45fde705d2cd6a4fed445f250b46202fe527
Vijay Chaudhary <bjchaudhary21@gmail.com>
2017-11-09 23:44:13 -0500

    Added algorithm to approach the problem and a few mips code to take user input

--- hexa-to-decimal.s
+++ hexa-to-decimal.s
@@ -5,3 +5,18 @@
 #3.1 Convert the hexadecimal to decimal. For eg: if string, s = "0af34e" 
 # decimal(s) = 0 * 16^5 + 10 * 16^4 + 15 * 16^3 + 3 * 16^2 + 4 * 16^1 + 14 * 16^0 
 #4 store it to a register and print out the value 
+
+.data 
+    prompt: .asciiz "Enter a hexa decimal number: " 
+.text
+    #prompt the user for input
+    li $v0, 4 
+    la $a0, prompt 
+    syscall
+
+    #get the user's input 
+    li $v0, 5 
+    syscall 
+
+    #store the result in $t0 
+    move $t0, $v0 
