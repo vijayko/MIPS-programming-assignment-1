71e7f2a730782f25f846f2aeeaeeff3b09569fc3
Vijay Chaudhary <bjchaudhary21@gmail.com>
2017-11-09 23:26:59 -0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MIPS-programming-assignment-1
+
+This is a MIPS program that reads a string of up to 8 characters from user input. If the string has only the characters from "0" to "9" and from 'a' to 'f' and from 'A' to 'F', the program prints out the unsigned decimal integer corresponding to the hexadecimal number in the string. Otherwise, the program prints out the message of "Invalid hexadecimal number." 
+
+
---
+++ hexa-to-decimal.s
@@ -0,0 +1,7 @@
+#1. Taking user input 
+#2.1 Iterate over the string. 
+#2.2 Make sure the strings contain '0' to '9', 'a' to 'f', 'A' to 'F'. If the string is valid, go to step 3, else throw a message "Invalid hexadecimal number"
+#3. If string is valid: 
+#3.1 Convert the hexadecimal to decimal. For eg: if string, s = "0af34e" 
+# decimal(s) = 0 * 16^5 + 10 * 16^4 + 15 * 16^3 + 3 * 16^2 + 4 * 16^1 + 14 * 16^0 
+#4 store it to a register and print out the value 
