10f4266de558ac1ef371b1d96d1f33d8138df7ee
Vijay Chaudhary <bjchaudhary21@gmail.com>
2017-11-15 21:53:52 -0500

    Implemented an almost-working code. Need to handle space cases and overflow case

--- hexa-to-decimal.s
+++ hexa-to-decimal.s
@@ -6,86 +6,99 @@
 # decimal(s) = 0 * 16^5 + 10 * 16^4 + 15 * 16^3 + 3 * 16^2 + 4 * 16^1 + 14 * 16^0 
 #4 store it to a register and print out the value 
 
-.data 
-	string: .asciiz "04567abcf"
-	array: .space 32
+.data
+	valid_array: .space 32
+	max_input: .space 9
+	error: .asciiz "\nInvalid Hexadecimal number"
 	newline: .asciiz "\n"
+	message: .asciiz "\nDone"
 .text
-	add $t0, $zero, 0 #index of the array
-	add $t1, $zero, 0 #index for the 16s
-	add $s0, $zero, 0 #total sum 
+main:
 	add $t7, $zero, 16
-	
-	la $s0, string
-	add $t0, $zero, 0
-	loop10: 
-		add $s1, $s0, $t0   
-    	lb $s2, 0($s1)      
-    	beq $s2, $zero, exit    #http://stackoverflow.com/questions/12739463/how-to-iterate-a-string-in-mips-assembly
-    	addi $t0, $t0, 1 
+	li $v0, 8  #taking the input
+    la $a0, max_input
+    li $a1, 9
+    syscall
+    
+    li $v0, 4
+    la $a0, max_input
+    
+    add $t0, $zero, 0 #iterator 
+    la $s0, max_input
+    
+    #checking if the input is valid
+    iterate: 
+    	add $s1, $s0, $t0   
+    	lb $s2, 0($s1)     #loading  
     	
-    	move $a0, $s2
-    	addi $v0, $zero, 11
-    	syscall
+    	#terminating the loop
+    	beq $s2, $zero, convert    #http://stackoverflow.com/questions/12739463/how-to-iterate-a-string-in-mips-assembly
+    	beq $s2, 10, convert
+    	addi $t0, $t0, 1 #iterator++
     	
-    	la $a0, newline
-    	addi $v0, $zero, 4
-    	syscall
-    	j loop10
-	exit:
-	add $t0, $zero, 0
-	loop0:
-		beq $t0, 32, exit0
-		li $v0, 5
+    	beq $s2, 32, iterate
+    	#checking if the number is in 0-9, a-f, A-F
+		sub $s3, $s2, 48 
+		
+		blt $s3, 0, exit
+		blt $s3, 10, store
+		sub $s3, $s3, 7
+		blt $s3, 10, exit
+		blt $s3, 16, store
+		sub $s3, $s3, 32
+		blt $s3, 10, exit
+		blt $s3, 16, store
+		bgt $s3, 15, exit
+		
+    	#addi $t0, $t0, 1 #iterator++
+    	
+    	j iterate
+   	
+    store: 
+    	# la $a0, convert_section
+    	# addi $v0, $zero, 4
+    	# syscall
+        #storing the valid digits to an array
+    	sw $s3, valid_array($t4)
+        bgt $t4, 32, exit3
+    	add $t4, $t4, 4 #iterator $t4 += 4
+
+    	# move $a0, $s3
+    	# addi $v0, $zero, 1
+    	# syscall
+    	j iterate
+    
+    add $t1, $zero, 0 #iterator 
+    add $t5, $zero, 0 #sum
+    convert: 
+        blt $t4, $zero, exit3
+        sub $t4, $t4, 4
+        lw $s4, valid_array($t4)
+
+        add $t2, $zero, 1 #16^x
+        add $t6, $zero, 0
+        loop2: 
+            beq $t1, $t6, out
+            multu $t2, $t7
+            mflo $t2
+            add $t6, $t6, 1
+            j loop2
+            
+        out: 
+        	add $t1, $t1, 1
+            multu $s4, $t2
+            mflo $s4
+            add $t5, $t5, $s4
+            j convert 
+    exit:
+        la $a0, error
+        addi $v0, $zero, 4
+        syscall 
+	exit3:	
+		la $a0, newline
+		li $v0, 4
+		syscall
+		li $v0, 1 
+		addi $a0, $t5, 0
 		syscall 
-		
-		move $t5, $v0
-		sw $t5, array($t0)
-		add $t0, $t0, 4  
-		
-		j loop0
-	exit0: 
-	add $t0, $zero, 0
-	loop1: 
-		beq $t0, 32, exit1
-		add $t6, $zero, 0 #counter for inner loop 
-		add $t4, $zero, 1 #temp value for 16^x 
-		
-		loop2: 
-			beq $t1, $t6, exit2 
-			mult $t4, $t7
-			mflo $t4
-			add $t6, $t6, 1 #increasing the inner counter 
-			
-			j loop2
-		exit2:
-		add $t1, $t1, 1
-		lw $t3, array($t0)
-		li $v0, 1 
-		addi $a0, $t3, 0 
-		syscall
-		
-		li $v0, 4 
-		la $a0, newline
-		syscall
-		
-		mult $t3, $t4 
-		mflo $t3 
-		add $s0, $s0, $t3
-		
-		li $v0, 1 
-		addi $a0, $s0, 0
-		syscall
-		li $v0, 4 
-		la $a0, newline
-		syscall
-		add $t0, $t0, 4
-		j loop1
-	exit1:
-	li $v0, 1 
-	addi $a0, $s0, 0
-	syscall 
-	
-	li $v0, 10 
-	syscall 
-	
+    
